31df7d833b0d900c94df30c1668c13d406bdc466
razzaksr <razzaksr@gmail.com>
2024-10-22 12:47:03 +0530

    22october2024

--- src/index.js
+++ src/index.js
@@ -1,14 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <div className='container'>
+    <div className='row justify-content-around'>
+      <button className='col-lg-2 col-md-6 col-12 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 col-md-6 col-12 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 col-md-12 col-12 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 col-md-12 col-12 btn btn-outline-primary' >HI</button>
+      </div>
+      <div className='row justify-content-around'>
+      <button className='col-lg-2 col-md-6 col-12 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 col-md-6 col-12 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 col-md-6 col-12 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 col-md-6 col-12 btn btn-outline-primary' >HI</button>
+      </div>
+    </div>
+{/*     
+    
+    
+    
+    
+    
+    
+    <div className='container bg-dark'>
+      <button className='btn' style={{color:'magenta', backgroundColor:'skyblue'}} >HI</button>
+      <button className='btn btn-primary' >HI</button>
+      <button className='btn btn-outline-primary' >HI</button>
+    </div>
+
+    <div className='container'>
+      <div className='row justify-content-center'>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      </div>
+      <div className='row justify-content-start'>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      </div>
+      <div className='row justify-content-end'>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      </div>
+      <div className='row justify-content-around'>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      </div>
+      <div className='row justify-content-between'>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      </div>
+      <div className='row justify-content-evenly'>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      <button className='col-lg-2 btn btn-outline-primary' >HI</button>
+      </div>
+    </div> */}
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
